1f2fb9c575dff03a8d5b58fc613831a26c1e5807
tapadar13 <monsurismyname@gmail.com>
2023-04-28 21:05:45 +0530

    promise chaining

--- Promises/script.js
+++ Promises/script.js
@@ -21,3 +21,17 @@ promise.then((success) => console, log(success));
 let promiseNew = new Promise(function (resolve, reject) {
   setTimeout(() => reject(new Error("Whoops!")), 1000);
 });
+
+//Promise Chaining
+
+let example = new Promise(function (resolve, reject) {
+  resolve("Promise resolved");
+});
+
+example
+  .then(function succesValue1(result) {
+    console.log(result);
+  })
+  .then(function successValue2() {
+    console.log("We can call multiple functions like this");
+  });
